d5dc205e2e1d6b42f3fb4874125a6563e6557f38
Antonyms01 <sohansarode922@gmail.com>
2024-06-18 22:55:41 +0530

    Day 2

---
+++ Day 2/yt.js
@@ -0,0 +1,20 @@
+const accid=10;
+let accname="Sohan";
+var acccity="bsl";
+//console.log(accid);
+//accid=12;
+// console.log(accid);                 //Not Allowed due to Const
+// accname="Rohan";
+// acccity="Mumbai";
+// console.table([accid,accname,acccity]);
+// console.log(typeof acccity);
+// console.log(typeof null);
+// console.log(typeof error);
+// console.log(typeof undefined);
+// let num=12;
+// let stringnum=String(num);
+// console.log(stringnum);
+// let booleannum=Boolean(num);
+// console.log(booleannum);
+console.log(1+"2"+2);
+
